c10cf4af422b6e3a53e46e13ec798e1a34d1baf3
Toaster Cardozo <14080501+iamthetoaster@users.noreply.github.com>
2023-06-26 10:27:10 -0700

    Added basic publishing script, minor restructuring

---
+++ publishing.el
@@ -0,0 +1,26 @@
+
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(
+        ("summer2023-notes"
+         :base-directory "./source/"
+         :base-extension "org"
+         :publishing-directory "./"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4             ; just the default for this project.
+         :auto-preamble t
+         ;; :auto-sitemap t
+         )
+        ("summer2023-static"
+         :base-directory "./source/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "./"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+        ("summer2023" :components ("summer2023-notes" "summer2023-static"))
+      ))
+
+;; (provide 'publishing)
+;;; publishing.el ends here
