d7e07c7ec688978a078162c8b7fac76faa3a1465
Toaster Cardozo <14080501+iamthetoaster@users.noreply.github.com>
2023-07-10 09:22:47 -0700

    Adjusted publishing script

--- publishing.el
+++ publishing.el
@@ -3,24 +3,19 @@
 (setq org-publish-project-alist
       '(
         ("summer2023-notes"
-         :base-directory "./source/"
+         :base-directory "~/org/lessons/summer2023/source/"
          :base-extension "org"
-         :publishing-directory "./"
+         :publishing-directory "~/org/lessons/summer2023/"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4             ; just the default for this project.
          :auto-preamble t
-         :auto-sitemap t
-         :sitemap-filename "index.org"
-         :sitemap-title "Lessons Summer of 2023"
-         :html-head "<link rel=\"stylesheet\" href=\"style.css\">"
-         :author "Gabriel Cardozo"
-         ; :property "header-args :results raw verbatim output :post output_drawer(data=*this*) :noweb strip-export" TODO: Think about how to make this work?
+          :auto-sitemap nil
          )
         ("summer2023-static"
-         :base-directory "./source/"
+         :base-directory "~/org/lessons/summer2023/source/"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "./"
+         :publishing-directory "~/org/lessons/summer2023/"
          :recursive t
          :publishing-function org-publish-attachment
          )
